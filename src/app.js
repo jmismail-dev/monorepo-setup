1c45b7dc2f967f9c82d7d35804d895f773ffc8ac
Mohamed Ismail <ismail@valorpaytech.com>
2022-05-09 18:51:27 +0530

    Changess

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const port = 7000
 app.get('/api', (req, res) => {
     res.send({
         message: 'Hello Мухаммед! ',
-        change : 'This is a change from us!!'
+        change : 'This is a change from us!!',
+        change2 : "This too"
     })
 })
 
